b32e78ed5986d12911e91abcbd0526a3a244b31c
Lennard Deurman <lennarddeurman@live.nl>
2022-11-01 17:02:24 +0100

    Added example project aand fix find element issue - MapifyGenerator took wrong input element - toList because we cannot assign an interable to a list - description / comment for sub dependencies

--- example/packages/example_data/lib/mappers/dtos/account.dart
+++ example/packages/example_data/lib/mappers/dtos/account.dart
@@ -3,6 +3,9 @@ import 'package:example_api/example_api.dart';
 import 'package:example_core/example_core.dart';
 import 'package:mapify/mapify.dart';
 
+/// Add child mapper files if there are dependencies
+import 'member.dart';
+
 part 'account.g.dart';
 
 @GenerateMapping(
